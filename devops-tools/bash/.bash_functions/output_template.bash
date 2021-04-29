bf66c717f496e7e74cadcd66123ccc9e2f9a8b7a
Andres Leon <andres.leon@navico.com>
2021-04-29 17:00:39 +1200

    added a template for printing messages to console
    system print line in console
    
    Signed-off-by: Andres Leon <andres.leon@navico.com>

---
+++ devops-tools/bash/.bash_functions/output_template.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+printf "==================================== \n\n"
+printf "\n\t Started ACTION \n\n"
+# commands and Ops
+printf "\n\t Finished ACTION \n\n"
+printf "==================================== \n\n"
